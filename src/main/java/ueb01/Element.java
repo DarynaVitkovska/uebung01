6dbbc892692047ce3fab10f998f1f8361dc758df
DarynaVitkovska <daryna_vitkovska@yahoo.com.ua>
2018-04-13 12:32:49 +0200

    Konstruktor hinzugefügt in die Klasse Element.

--- src/main/java/ueb01/Element.java
+++ src/main/java/ueb01/Element.java
@@ -3,4 +3,9 @@ package ueb01;
 public class Element {
     public char w;
     public Element next;
+
+    public Element(char w, Element next){
+        this.w = w;
+        this.next = next;
+    }
 }
